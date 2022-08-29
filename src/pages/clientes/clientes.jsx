5edc4d82e82da31feb9055ab3db3a68647c93e5f
NodjaLima <nodjamplima@gmail.com>
2022-08-29 12:06:25 -0300

    Corrige estilização de clientes

--- src/pages/clientes/clientes.jsx
+++ src/pages/clientes/clientes.jsx
@@ -12,11 +12,14 @@ const Clientes = () => {
     return ( 
     <div className="clientes-container">
       <h1>Lista de Clientes Cadastrados</h1>
+      <div>
       {clientes.map((cliente, index) => {
         return (
           <Link to={`/cliente/${index + 1}`} ><p>{`Cadastro ${index + 1}: ${cliente.nome} ${cliente.sobrenome}`}</p></Link>
         )
       })}
+      </div>
+      
     </div>
    )
 }
